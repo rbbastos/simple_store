aeb4c0d190212a4f9e9a2719183151f7a941321c
rbbastos <bastoscanada@gmail.com>
2019-10-02 18:29:35 -0500

    Generated FaKe data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-p = Product.create
+Product.destroy_all
+
+676.times do
+  prod = Product.create(title: Faker::Commerce.product_name,
+                        price: Faker::Commerce.price,
+                        stock_quantity: Faker::Number.within(range: 1..1000))
+end
+
+puts "Generated #{Product.count} products."
